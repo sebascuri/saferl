bda32e8eb9661d58c9d63303612dc7aa0925b8ea
scuri <sebastian.curi@inf.ethz.ch>
2021-10-25 13:28:24 +0200

    Init repo

---
+++ README.md
@@ -0,0 +1,18 @@
+# Implementation of H-ORL Algorithm
+
+[![CircleCI](https://img.shields.io/circleci/build/github/sebascuri/hucrl/master?label=master%20build%20and%20test&token=7f83bf4eea065c55015a2685c2b0ffbf996e3a2a)](https://app.circleci.com/pipelines/github/sebascuri/hucrl)
+[![CircleCI](https://img.shields.io/circleci/build/github/sebascuri/hucrl/dev?label=dev%20build%20and%20test&token=7f83bf4eea065c55015a2685c2b0ffbf996e3a2a)](https://app.circleci.com/pipelines/github/sebascuri/hucrl)
+[![Code style: black](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/psf/black)
+[![License](https://img.shields.io/github/license/mashape/apistatus.svg)](https://pypi.org/project/hug/)
+
+
+
+To install create a conda environment:
+```bash
+$ conda create -n saferl python=3.7
+$ conda activate saferl
+```
+
+```bash
+$ pip install -e .[test]
+```
---
+++ setup.py
@@ -0,0 +1,49 @@
+"""Setup script of saferl."""
+from setuptools import find_packages, setup
+
+extras = {
+    "test": [
+        "pytest>=5.0,<5.1",
+        "flake8>=3.7.8,<3.8",
+        "pydocstyle==4.0.0",
+        "black>=19.10b0",
+        "isort>=5.0.0",
+        "pytest_cov>=2.7,<3",
+        "mypy==0.750",
+    ],
+    "envs": [
+        "box2d-py>=2.3.5",
+        "atari_py>=0.2.6",
+        "MinAtar @ git+ssh://git@github.com/kenjyoung/MinAtar@master#egg=MinAtar",
+        "seaborn>=0.9.0",
+        # "gym-minigrid>=1.0",
+    ],
+}
+extras["all"] = [item for group in extras.values() for item in group]
+
+setup(
+    name="saferl",
+    version="0.0.1",
+    author="Sebastian Curi",
+    author_email="sebascuri@gmail.com",
+    license="MIT",
+    python_requires=">=3.7.0",
+    packages=find_packages(exclude=["docs"]),
+    install_requires=[
+        "rllib @ git+ssh://git@github.com/sebascuri/rllib@dev#egg=rllib",
+        "hucrl @ git+ssh://git@github.com/sebascuri/hucrl@dev#egg=hucrl",
+        "safety_gym @ git+ssh://git@github.com/openai/safety-gym#egg=safety_gym"
+    ],
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        "Development Status :: 3 - Alpha",
+        # Indicate who your project is intended for
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.7",
+    ],
+)
